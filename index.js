a4341c68d42c98728e2f0572bb48a19145805090
femilsabahaya <femilsabhaya23@gmail.com>
2022-11-03 09:21:20 +0530

    remove yellow box

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
  * @format
  */
 
-import {Alert, AppRegistry} from 'react-native';
+import {Alert, AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import 'react-native-gesture-handler';
 import i18n from './lang/i18n';
 
 import {name as appName} from './app.json';
+
+// LogBox.ignoreAllLogs();
 
 AppRegistry.registerComponent(appName, () => App);
